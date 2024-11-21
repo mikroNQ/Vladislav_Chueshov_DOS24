f9ca9eb8b65e3fc2d05089bb8cab113054dfb93c
mikroNQ <mikron97@gmail.com>
2024-11-21 18:01:26 +0300

    improved the script lesson 11 task_1

--- Homework_Lesson11_Bash\Shell_1/find_files.sh
+++ Homework_Lesson11_Bash\Shell_1/find_files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 3 ]; then
+    echo "Ошибка: требуется три аргумента"
+    echo "Использование: $0 выходной_файл директория расширение"
+    echo "Пример: $0 res.txt ~/Desktop png"
+    exit 1
+fi
+
+if [ ! -d "$2" ]; then
+    echo "Ошибка: директория '$2' не существует"
+    exit 1
+fi
+
 find "$2" -maxdepth 1 -type f -name "*.$3" | while read -r file; do
     basename "$file"
 done > "$1"
